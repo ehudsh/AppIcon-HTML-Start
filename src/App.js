2509028066bf227c65524326c502beb8e689a917
ehudsh <ehudshulman@gmail.com>
2020-11-03 10:46:53 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppIcon from "./Components/AppIcon/AppIcon";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <AppIcon
+ 
+      />  */}
+      <AppIcon />
+    </div>
+  );
+}
